913f13b2d8549a11b47496bea9989860368be622
David <david_@gmx.net>
2018-01-16 11:56:14 +0100

    fixed merge

--- Übungen/Übung 2/VIS-Ue2-KrögD-BenjaminH-Vers-1/src/at/fhooe/mc/server/Server.java
+++ Übungen/Übung 2/VIS-Ue2-KrögD-BenjaminH-Vers-1/src/at/fhooe/mc/server/Server.java
@@ -13,52 +13,19 @@ public class Server extends UnicastRemoteObject implements IEnvService {
 
 	protected Server() throws RemoteException {
 		super();
-<<<<<<< HEAD
 	}
 
-	public static void main(String[] args) {
-		try {
-			Server server = new Server();
-			Registry reg = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
-			reg.rebind("EnvService", server);
-		} catch (Exception _e) {
-			_e.printStackTrace();
-		}
-=======
->>>>>>> 6181db151770fabcd0f3e5b70b2f3b357ad504ef
-
-	}
 
 	@Override
 	public EnvData requestEnvironmentData(String _type) throws RemoteException {
-		char[] buffer = new char[100];
 		EnvData data = null;
-<<<<<<< HEAD
+
 		if (_type.equals("air")) {
 			data = new AirData();
 		} else {
 			System.out.println("Error");
 		}
-=======
-		StringBuilder stringbuilder = new StringBuilder();
-		stringbuilder.append("sensor;" + _type + "#\0");
-		mOutput.println(stringbuilder.toString());
-		try {
-			mInput.read(buffer);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String msg = new String(buffer).split("#")[0];
-		System.out.println(msg);
-		if (_type.equals("air")) {
-			String[] splitMsg = msg.split("\\|");
-			String[] messageVal = splitMsg[1].split(";");
-			data = new AirData(Integer.parseInt(splitMsg[0]), Integer.parseInt(messageVal[0]),
-					Integer.parseInt(messageVal[1]));
-		} else {
-			System.out.println("Error");
-		}
->>>>>>> 6181db151770fabcd0f3e5b70b2f3b357ad504ef
+
 		return data;
 	}
 
