a80fa382603062f127fc4ca84ccdbd6ff8f9230b
chainstair <benjamin.gezgin@hotmail.com>
2018-01-16 16:46:03 +0100

    added HelloWorldServlet

--- Übungen/Übung 2/VIS-Ue2-KrögD-BenjaminH-Vers-1/src/at/fhooe/mc/server/HelloWorldServlet.java
+++ Übungen/Übung 2/VIS-Ue2-KrögD-BenjaminH-Vers-1/src/at/fhooe/mc/server/HelloWorldServlet.java
@@ -1,5 +1,21 @@
 package at.fhooe.mc.server;
 
-public class HelloWorldServlet {
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
+@WebServlet("/helloWorld!")
+public class HelloWorldServlet extends HttpServlet {
+	public void doGet (HttpServletRequest _request, HttpServletResponse _response) throws ServletException, IOException{
+				_response.setContentType("text/html");
+				PrintWriter out = _response.getWriter();
+				out.println("<HTML>");
+				out.println("<HEAD><TITLE>HelloWorld</TITLE></HEAD>");
+				out.println("<BODY>");
+				out.println("<H1>Hello World!</H1>");
+				out.println("<H2>Aufrufe: </H2>");
+				out.println("</BODY>");
+				out.println("</HTML>");
+		}
 }
