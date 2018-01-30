d390c1a39831b0db8da08cef94c67ccf0420b241
David Krög <david_@gmx.net>
2018-01-30 14:37:53 +0100

    added android project

---
+++ AndroidBTApp/app/src/main/java/at/fhooe/mc/android/btleproject/DevicesListActivity.java
@@ -0,0 +1,13 @@
+package at.fhooe.mc.android.btleproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DevicesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devices_list);
+    }
+}
