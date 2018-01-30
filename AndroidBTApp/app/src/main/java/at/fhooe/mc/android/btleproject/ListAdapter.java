75eedf9725ec540d4f780a69c44aded81464102e
David Krög <david_@gmx.net>
2018-01-30 17:16:49 +0100

    created java project

---
+++ AndroidBTApp/app/src/main/java/at/fhooe/mc/android/btleproject/ListAdapter.java
@@ -0,0 +1,13 @@
+package at.fhooe.mc.android.btleproject;
+
+import android.app.Application;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by David on 30.01.18.
+ */
+
+
--- Übungen/Übung 4/VISUe4KrgDHadodoBVers1/app/src/main/java/at/fhooe/mc/vis_ue4_krgd_hadodob_vers_1/BluetoothModel.kt
+++ Übungen/Übung 4/VISUe4KrgDHadodoBVers1/app/src/main/java/at/fhooe/mc/vis_ue4_krgd_hadodob_vers_1/BluetoothModel.kt
@@ -3,7 +3,7 @@ package at.fhooe.mc.vis_ue4_krgd_hadodob_vers_1
 /**
  * Created by chainstair on 29.01.2018.
  */
-class BluetoothModel(name : String, sensordata : String) {
+class sBluetoothModel(name : String, sensordata : String) {
 
     var name : String = name
     var sensordata : String = sensordata
